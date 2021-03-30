1794b53d9531f4d0de8751c3276e6f605aea9b67
yukasaito718 <bumbumbanana718@gmail.com>
2021-03-30 15:57:35 +0900

    test

--- database/migrations/2021_03_14_223108_add_votes_to_contact_forms_table.php
+++ database/migrations/2021_03_14_223108_add_votes_to_contact_forms_table.php
@@ -15,13 +15,13 @@ class AddVotesToContactFormsTable extends Migration
     {
         Schema::table('contact_forms', function (Blueprint $table) {
             $table->string('your_name', 20)->after('id');
-            $table->string('email', 255)->after('your_name');
+            $table->string('email', 191)->after('your_name');
             $table->longText('url')->nullable($value = true)->after('email');
             $table->boolean('gender')->after('url');
             $table->tinyInteger('age')->after('gender');
-            $table->string('contact', 200)->after('age');
-            $table->string('title', 255)->after('contact');
-            $table->boolean('caution', 255)->after('title');
+            $table->string('contact', 191)->after('age');
+            $table->string('title', 191)->after('contact');
+            $table->boolean('caution', 191)->after('title');
         });
     }
 
